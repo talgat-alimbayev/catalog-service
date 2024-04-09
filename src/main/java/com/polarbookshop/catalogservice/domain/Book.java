d745d9e0413f2427098db00fef0a92564ff12cb9
Talgat Alimbayev <>
2024-04-10 01:17:10 +0500

    basic business logic

---
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -0,0 +1,7 @@
+package com.polarbookshop.catalogservice.domain;
+
+public record Book(String isbn,
+                   String title,
+                   String author,
+                   Double price) {
+}
