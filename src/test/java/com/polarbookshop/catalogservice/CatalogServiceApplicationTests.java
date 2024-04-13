a352fbb7ac2ad6fd132a5e10daa5ac66c6414238
Talgat Alimbayev <>
2024-04-13 22:51:16 +0500

    ci cd pipeline setup

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
 
+    @Test
+    void whenPostRequestThenBookCreated() {
+        var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+
+        webTestClient.post().uri("/books").bodyValue(expectedBook).exchange().expectStatus().isCreated().
+            expectBody(Book.class).value(actualBook -> {
+                assertThat(actualBook).isNotNull();
+                assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+            });
+    }
 }
